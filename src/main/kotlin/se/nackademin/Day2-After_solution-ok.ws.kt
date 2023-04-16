e322e94d8e40839142ad21fcc6aa6a1f2a177748
BaharehTorki <bahareh.torki@yh.nackademin.se>
2023-04-16 23:26:50 +0200

    Write comments about codes that what they do

--- src/main/kotlin/se/nackademin/Day2-After_solution-ok.ws.kt
+++ src/main/kotlin/se/nackademin/Day2-After_solution-ok.ws.kt
@@ -15,7 +15,7 @@ fun main() {
 }
 
 //Modifier 'Data' för lagring av lösenordspolicy
-//Dataklassen instruerar kompilatorn att generera ett gäng användbara metoder för denna klass som: constructor, equals, hasCode och toString
+//Dataklassen instruerar till compiler att generera ett gäng användbara metoder för denna klass som: constructor, equals, hasCode och toString
 data class PasswordWithPolicy(
     val password: String,
     //typ IntRange för att lagra en integer range
@@ -23,66 +23,56 @@ data class PasswordWithPolicy(
     val letter: Char,
 ) {
 
-    //We count the occurrences of 'letter' {it} in the password String, and check tha the result is in range
-    fun validatePartOne() =
-
-        //'in' checks that the given element belongs to a range
-        //for numbers and other comparable elements, 'x in range' is the same as writing the following explicit check range.first <= x && x >= range.last
-        password.count { it == letter } in range
-
-    //WE subtract 1 from 'range.first' because the policy starts indexing at one, but String indexation starts at zero
-    fun validatePartTwo() =
-        (password[range.first - 1] == letter) xor (password[range.last - 1] == letter)
-
-
-    //parses one input line into PasswordWithPolicy, since it is function that creates an instance
-    //provide the parameter names explicitly
+    //Första sättet för att konvertera input
+    //Omvandla en input rad i PasswordWithPolicy, eftersom det är funktionen som skapar en instans
     companion object {
-        //parse function return 'PasswordWithPolicy'
+        //parse funktion returnerar 'PasswordWithPolicy'
         fun parse(line: String) = PasswordWithPolicy(
+
             //password including String before colon
             password = line.substringAfter(": "),
 
-            //If input is empty or contains more characters, 'singleOrNull' returns null.
+            //'singleOrNull' returnerar null om input är tom eller innehåller fler tecken.
             letter = line.substringAfter(" ").substringBefore(":").single(),
 
-            //range goes before the whitespace and consists of two numbers split by the hyphen
-            //First substring before the whitespace
-            //call 'let' to introducing a new variable inside a lambda and later return the desired range as the result
-            range = line.substringBefore(" ").let {
+            //Range går före blanktecken och består av två tal delade med bindestreck
+            range = line.substringBefore(" ").let
 
-                //split the substring returned by the 'substringBefore' with hyphen
-                //split returns a list of Strings as a result
+            //vi använder syntax för 'destructuring declaration' för att tilldela resultatet till två variabler och returneras det önskade range som resultat
+            {
                 val (start, end) = it.split("-")
-
-                //we use destructuring declaration syntax to assign the result into two variables
                 start.toInt()..end.toInt()
             },
         )
 
-        //We convert both lines to 'Int's and build a range using the 'rangeTo' operator
-
-        //a regular expression describing the input format on the screen right now
-        //First go two groups of numbers split with the hyphen, then a letter and then a group of characters by 'Regex' class for define a regular expression
+        //Andra sättet för att konvertera input
+        //Ett uttryck som beskriver delade siffror med bindestrecket, en bokstav och en grupp tecken för att definiera ett regelrätt uttryck
         private val regex = Regex("""(\d+)-(\d+) ([a-z]): ([a-z]+)""")
 
-        //parseUsingRegex that should match the line against a regular expression and then build 'PasswordWithPolicy´on the result
+        //parseUsingRegex funktion som ska matcha raden mot ett regelrätt uttryck och sedan bygger 'PasswordWithPolicy'på resultatet
         fun parseUsingRegex(line: String): PasswordWithPolicy =
 
-            //match entire function that tries to match the given input against the regex pattern.
-            // If the attempt is unsuccessful and input doesn't correspond to the pattern, 'matchEntire' returns 'null'
+            //matchEntire funktionen försöker att matcha den givna inmatningen mot regex pattern. Om försöket misslyckas, returneras "null"
             regex.matchEntire(line)!!
 
-                    //destructured property returns these components ready for destructuring assignment
+                //destructured property returnerar dessa komponenter redo för destructuring assignment
                 .destructured
 
-                //and then we use its result together with 'let' and destruct it inside the lambda expression
-                //parameters corresponding to groups in a regular expression and
-                // finally we build the passwordWithPolicy object.
+                //Att använda dess resultat i en lambda uttryck och parametrar som anpassar grupper i uttrycket och sedan bygger vi passwordWithPolicy-objektet.
                 .let { (start, end, letter, password) ->
 
-                    //Converting 'start' and 'end' String to Int and extract a single 'Char' from the 'letter' String
+                    //Konvertera "start" och "end" sträng och extrahera "Char"
                     PasswordWithPolicy(password, start.toInt()..end.toInt(), letter.single())
                 }
     }
+
+    //Räknas upprepningar av 'bokstav' {it} och kontrollerar att resultatet är inom range
+    fun validatePartOne() =
+        password.count { it == letter } in range
+
+    //Subtraheras 1 från 'range.first' eftersom policyn börjar indexera vid ett, men strängindexering börjar på noll
+    fun validatePartTwo() =
+        (password[range.first - 1] == letter) xor (password[range.last - 1] == letter)
+
+
 }
